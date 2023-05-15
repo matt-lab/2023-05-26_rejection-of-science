0c1749decbf40ba91460e057154670998685774b
Matt Andreotta <matthewandreotta@gmail.com>
2023-05-15 11:56:01 +0800

    init

---
+++ scripts/generate_meta.R
@@ -0,0 +1,16 @@
+#### 📚 Load libraries----------------------------------------------------------
+library(metathis)
+
+#### ✍️ Add metadata-----------------------------------------------------------
+metathis::meta() |>
+    metathis::meta_social(
+    title = "The psychological roots of the rejection (and acceptance) of science",
+    description = "A workshop exploring the psychological determinants of belief in science",
+    url = ".../#/", # direct link to presentation slides 
+    image = ".../index-img.png", # link to screenshot of title slide
+    image_alt = "The title slide of the presentation",
+    twitter_card_type = "summary_large_image",
+    twitter_creator = "@MattAndreotta",
+    og_locale = "en_AU"
+    ) |>
+    write_meta("meta.html")
