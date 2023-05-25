8175196451fd5762c2f9ed7b2ecbf03fb312dafa
Matt Andreotta <matthewandreotta@gmail.com>
2023-05-25 17:15:47 +1000

    Updated meta and coan error

--- scripts/generate_meta.R
+++ scripts/generate_meta.R
@@ -6,7 +6,7 @@ metathis::meta() |>
     metathis::meta_social(
     title = "The psychological roots of the rejection (and acceptance) of science",
     description = "A workshop exploring the psychological determinants of belief in science",
-    url = "https://matt-lab.github.io/2023-05-26_rejection-of-science", # direct link to presentation slides 
+    url = "https://matt-lab.github.io/workshop_cms-rejection-of-science", # direct link to presentation slides 
     image = ".../index-img.png", # link to screenshot of title slide
     image_alt = "The title slide of the presentation",
     twitter_card_type = "summary_large_image",
